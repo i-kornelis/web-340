3cb63b730cfcf89935170654936767b2bee25a37
i-kornelis <izabellakornelis@gmail.com>
2020-02-20 18:37:02 -0800

    Adding Exercise 1.3

---
+++ week-1/kornelis-exercise-1.3.js
@@ -0,0 +1,33 @@
+/*
+============================================
+; Title: Exercise 1.1
+; Author: Izabella Kornelis
+; Date: 19 February 2020
+; Modified By: Izabella Kornelis
+; Description: A simple JavaScript program using Node.js to work with modules.
+;===========================================
+*/
+
+// Require statement that imports the header.js file from my root directory.
+const header = require('../kornelis-header.js');
+
+// Call the console.log() function and output a well-formatted header
+console.log(header.display("Izabella", "Kornelis", "Exercise 1.1"));
+// new line
+console.log('\n');
+
+//program start
+
+var url = require('url')
+
+var parsedURL = url.parse("https://www.example.com/profile?name=kornelis");
+
+// output
+console.log(parsedURL.protocol);
+
+console.log(parsedURL.host);
+
+console.log(parsedURL.query);
+
+
+//end program
