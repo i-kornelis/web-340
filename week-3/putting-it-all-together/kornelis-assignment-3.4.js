3cc27b604d361c75610d77e165d691f53483b421
i-kornelis <izabellakornelis@gmail.com>
2020-03-07 14:20:20 -0800

    adding assignment 3.4

---
+++ week-3/putting-it-all-together/kornelis-assignment-3.4.js
@@ -0,0 +1,62 @@
+/*
+============================================
+; Title: Assignment 3.4
+; Author: Izabella Kornelis
+; Date: March 5 2020
+; Modified By: Izabella Kornelis
+; Description: Demonstrates how to us advanced routing in Express Framework.
+;===========================================
+*/
+
+// Require statement that imports the header.js file from my root directory.
+const header = require('../../kornelis-header.js');
+
+// Call the console.log() function and output a well-formatted header
+console.log(header.display("Izabella", "Kornelis", "Assignment 3.4"));
+// new line
+console.log('\n');
+
+// program start
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var logger = require("morgan");
+
+// start Express
+var app = express()
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("short"));
+
+app.get("/", function (request, response) {
+  response.render("index", {
+    message: "home page"
+  });
+});
+
+app.get("/about", function (request, response) {
+  response.render("about", {
+    message: "about page"
+  });
+});
+
+app.get("/contact", function (request, response) {
+  response.render("contact", {
+    message: "contact page"
+  });
+});
+
+app.get("/products", function (request, response) {
+  response.render("products", {
+    message: "products page"
+  });
+});
+
+// create serve on port:8080
+http.createServer(app).listen(8080, function () {
+  console.log("Application started on port 8080.")
+});
+
+// end program
